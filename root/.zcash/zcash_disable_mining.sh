800606161ad4c9598df2e46852a23adecca1e529
andyceo <andyceo@yandex.ru>
2016-11-05 00:54:30 +0300

    Several bugfixes. Adjust cron time.

--- root/.zcash/zcash_disable_mining.sh
+++ root/.zcash/zcash_disable_mining.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sed -i -e 's/gen=1/gen=0/g' zcash.conf
+sed -i -e 's/gen=1/gen=0/g' /root/.zcash/zcash.conf
 zcash-cli stop
 sleep 2
 zcashd --daemon
--- root/.zcash/zcash_enable_mining.sh
+++ root/.zcash/zcash_enable_mining.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-sed -i -e 's/gen=0/gen=1/g' zcash.conf
+sed -i -e 's/gen=0/gen=1/g' /root/.zcash/zcash.conf
 zcash-cli stop
 sleep 2
 zcashd --daemon
